2fa4cc6e6218bdb215590e8a34a8a414779681c9
Britt Evans <bevans@biocorellc.com>
2024-03-27 10:46:04 -0400

    Add DAPI percentage charts

--- app.py
+++ app.py
@@ -2,20 +2,27 @@ import streamlit as st
 from pathlib import Path
 
 import cell_stats
+import charts
 
+st.set_page_config(layout="wide")
 st.title("Cell Count Stats")
 uploaded_file = st.file_uploader("Choose a spreadsheet")
 if uploaded_file is not None:
     my_stats = cell_stats.compute_stats(uploaded_file)
     my_output = cell_stats.gen_excel_output(my_stats)
-    st.dataframe(
-        my_stats.top_output.to_pandas(use_pyarrow_extension_array=True),
-        use_container_width=True
-    )
     st.download_button(
         label="Click to Download Full Results!",
         data=my_output.getvalue(),
         file_name=Path(uploaded_file.name).stem + "_results.xlsx",
         mime="application/vnd.ms-excel"
+    )
+    st.dataframe(
+        my_stats.top_output.to_pandas(use_pyarrow_extension_array=True),
+        use_container_width=True
+    )
+    st.header("Percent of DAPI Charts")
+    st.plotly_chart(
+        charts.dapi_percents(my_stats),
+        use_container_width=True
     )
 
---
+++ charts.py
@@ -0,0 +1,39 @@
+import plotly.express as px
+import plotly.graph_objects as go
+
+import cell_stats
+
+
+def dapi_percents(my_stats: cell_stats.CellStats,
+                  n_cols: int = 4) -> go.Figure:
+    labels_melted = my_stats.mouse_labels.transpose(
+        include_header=True, header_name="mouse", column_names=" "
+    )
+
+    dapi_percents = my_stats.top_as_percent.melt(
+        id_vars="Category", value_name="percent", variable_name="mouse"
+    ).join(labels_melted, on="mouse")
+
+    n_rows = dapi_percents["Category"].n_unique() // n_cols + 1
+
+    fig = px.box(
+        dapi_percents,
+        y="percent",
+        color="Genotype",
+        facet_col="Category",
+        x="Gender",
+        facet_col_wrap=n_cols,
+        points="all",
+        facet_col_spacing=0.05,
+    )
+    fig.update_yaxes(
+        title="",
+        tickformat=".0%",
+        matches=None,
+        showticklabels=True,
+        rangemode="tozero",
+    )
+    fig.update_layout(
+        height=n_rows * 300
+    )
+    return fig
